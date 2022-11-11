70b9d3ff25a51b2b50f7c4c16ca25cb4c0ffee5d
Yofou <exodias1965@gmail.com>
2022-11-11 20:19:18 +0000

    feat: init template

---
+++ packages/ui/src/components/Counter.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let count = 0
+	
+	const inc = () => count++
+	const dec = () => count--
+</script>
+
+<div>
+	<button on:click={dec}>dec</button>
+	<p>{count}</p>
+	<button on:click={inc}>inc</button>
+</div>
+
+<style>
+	div {
+		display: flex;
+	}
+</style>
